12b18968e191968ae6075c4a06a4713991da2157
Maya Erez <merez@codeaurora.org>
2012-09-29 06:30:47 +0200

    mmc: core: Add the ability to enable BKOPS
    If the card and the host support BKOPS, and BKOPS is not enabled
    yet, set the BKOPS_EN bit to enable BKOPS.
    This bit is one time programmable.
    
    Change-Id: I9211f68cf7a6ba7e57964a5acb908efa9a9e515a
    Signed-off-by: Maya Erez <merez@codeaurora.org>

--- include/linux/mmc/host.h
+++ include/linux/mmc/host.h
@@ -246,6 +246,7 @@ struct mmc_host {
 #define MMC_CAP2_PACKED_CMD	(MMC_CAP2_PACKED_RD | \
 				 MMC_CAP2_PACKED_WR) /* Allow packed commands */
 #define MMC_CAP2_SANITIZE	(1 << 13)		/* Support Sanitize */
+#define MMC_CAP2_INIT_BKOPS	    (1 << 15)	/* Need to set BKOPS_EN */
 #define MMC_CAP2_POWER_OFF_VCCQ_DURING_SUSPEND	(1 << 16)
 
 	mmc_pm_flag_t		pm_caps;	/* supported pm features */
