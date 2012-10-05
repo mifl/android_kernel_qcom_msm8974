7be03077eccf81780b2010759994da5540ad47cf
Oluwafemi Adeyemi <aadeyemi@codeaurora.org>
2012-10-04 17:57:14 -0700

    mmc: msm_sdcc: Remove unused flag variables
    Remove unused variables causing compile error
    when RUNTIME_PM is disabled.
    
    Change-Id: I7cd362105b04eb8c510fc63407e205f9ccd5db49
    Signed-off-by: Oluwafemi Adeyemi <aadeyemi@codeaurora.org>

--- drivers/mmc/host/msm_sdcc.c
+++ drivers/mmc/host/msm_sdcc.c
@@ -3488,7 +3488,6 @@ static int msmsdcc_enable(struct mmc_host *mmc)
 {
 	struct device *dev = mmc->parent;
 	struct msmsdcc_host *host = mmc_priv(mmc);
-	unsigned long flags;
 	int rc = 0;
 
 	msmsdcc_pm_qos_update_latency(host, 1);
@@ -3522,7 +3521,6 @@ out:
 static int msmsdcc_disable(struct mmc_host *mmc)
 {
 	struct msmsdcc_host *host = mmc_priv(mmc);
-	unsigned long flags;
 	int rc = 0;
 
 	msmsdcc_pm_qos_update_latency(host, 0);
