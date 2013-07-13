96295c105428256b3bfa2d50aae0529d0d1d1bc7
Eunchul Kim <chulspro.kim@samsung.com>
2013-07-13 14:39:46 +0900

    kgsl/drm: Remove dependency with kgsl-drm
    This patch removes dependency with kgsl-drm for irq feature enable.
    I seperate kgsl-drm from kgsl for platform device.
    
    Change-Id: I42f55c2e8ef0f8a519c887c07c200131c62d4e22
    Signed-off-by: Eunchul Kim <chulspro.kim@samsung.com>
    Signed-off-by: Mathew Karimpanal <kjmathew@codeaurora.org>

--- drivers/gpu/msm/kgsl.c
+++ drivers/gpu/msm/kgsl.c
@@ -3972,7 +3972,6 @@ static int _register_device(struct kgsl_device *device)
 
 int kgsl_device_platform_probe(struct kgsl_device *device)
 {
-	int result;
 	int status = -EINVAL;
 	struct resource *res;
 	struct platform_device *pdev =
@@ -4076,11 +4075,6 @@ int kgsl_device_platform_probe(struct kgsl_device *device)
 		device->reg_virt);
 
 	rwlock_init(&device->context_lock);
-
-	result = kgsl_drm_init(pdev);
-	if (result)
-		goto error_pwrctrl_close;
-
 
 	setup_timer(&device->idle_timer, kgsl_timer, (unsigned long) device);
 	status = kgsl_create_device_workqueue(device);
@@ -4227,7 +4221,6 @@ static void kgsl_core_exit(void)
 	kgsl_mmu_ptpool_destroy(kgsl_driver.ptpool);
 	kgsl_driver.ptpool = NULL;
 
-	kgsl_drm_exit();
 	kgsl_cffdump_destroy();
 	kgsl_core_debugfs_close();
 
