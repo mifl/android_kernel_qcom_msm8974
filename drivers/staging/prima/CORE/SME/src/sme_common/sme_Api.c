e8179c32a63249767a6318f11c4b0c8e5e326d53
sultanxda <sultanxda@gmail.com>
2015-10-26 22:44:04 +0100

    prima: hax to make kk prima work on lollipop kernel
    Change-Id: I7ca6c86549881278cd4f63b4b1641f3df4aa4d30

--- drivers/staging/prima/CORE/SME/src/sme_common/sme_Api.c
+++ drivers/staging/prima/CORE/SME/src/sme_common/sme_Api.c
@@ -9585,7 +9585,7 @@ VOS_STATUS sme_SendTdlsLinkEstablishParams(tHalHandle hHal,
     \- return VOS_STATUS_SUCCES
     -------------------------------------------------------------------------*/
 VOS_STATUS sme_SendTdlsMgmtFrame(tHalHandle hHal, tANI_U8 sessionId, tSirMacAddr peerMac,
-      tANI_U8 frame_type, tANI_U8 dialog, tANI_U16 statusCode, tANI_U8 *buf, tANI_U8 len, tANI_U8 responder)
+      tANI_U8 frame_type, tANI_U8 dialog, tANI_U16 statusCode, tANI_U32 peerCapability, tANI_U8 *buf, tANI_U8 len, tANI_U8 responder)
 {
     eHalStatus          status    = eHAL_STATUS_SUCCESS;
     tCsrTdlsSendMgmt sendTdlsReq = {{0}} ;
@@ -9601,6 +9601,7 @@ VOS_STATUS sme_SendTdlsMgmtFrame(tHalHandle hHal, tANI_U8 sessionId, tSirMacAddr
         sendTdlsReq.dialog = dialog;
         sendTdlsReq.statusCode = statusCode;
         sendTdlsReq.responder = responder;
+        sendTdlsReq.peerCapability = peerCapability;
 
         status = csrTdlsSendMgmtReq(hHal, sessionId, &sendTdlsReq) ;
 
